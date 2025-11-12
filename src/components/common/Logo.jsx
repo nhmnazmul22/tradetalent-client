3d5b24e3bd40c7f92e53ffb8191aed4b21a2bdf5
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-11-12 09:42:32 +0600

    updated logo

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -4,10 +4,10 @@ import React from "react";
 const Logo = () => {
   return (
     <div className="text-black dark:text-white flex items-center gap-2">
-      <span className="inline-block bg-linear p-1 rounded-lg text-white">
-        <Laptop size={32} className=""></Laptop>
+      <span className="inline-block bg-linear p-1 rounded text-white">
+        <Laptop size={24} className=""></Laptop>
       </span>
-      <h2 className="text-3xl font-bold">
+      <h2 className="text-2xl font-bold">
         Trade<span className="italic">Talent</span>
       </h2>
     </div>
