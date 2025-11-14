e9c7f9c189c86f807c821a090c2294fc33b0c15d
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-11-14 09:48:56 +0600

    refactor Hero component layout and styling for improved responsiveness

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,58 +7,55 @@ import { fadeInUp, staggerContainer } from "@/lib/motionVariants";
 
 const Hero = () => {
   return (
-    <section
-      className={`relative min-h-[95vh] h-full py-10 bg-[url(/images/hero-bg-2.jpg)] dark:bg-[url(/images/hero-bg.jpg)] bg-center bg-cover bg-no-repeat bg-blend-soft-light dark:scale-x-[-1] `}
-    >
-      <div class="absolute inset-0 bg-linear-to-r  from-black/80 to-black/20"></div>
-      <figure className="absolute bottom-[-130px] left-0 z-10 w-full dark:scale-x-[-1] ">
-        <img
-          src={WaveIllustration}
-          alt="Bg wave"
-          className="w-full object-cover"
-        />
-      </figure>
-      <div className="main-container relative z-20 dark:scale-x-[-1]">
-        <motion.div
-          variants={staggerContainer}
-          initial="start"
-          animate="end"
-          className="max-w-[700px] w-full my-20"
-        >
-          <motion.h1
-            variants={fadeInUp}
-            transition={{ duration: 0.6 }}
-            className="text-7xl font-semibold text-white mb-2"
-          >
-            Find Talent.{" "}
-            <span className="text-indigo-600 italic">Build Success</span>.
-            Faster.
-          </motion.h1>
-          <motion.p
-            variants={fadeInUp}
-            transition={{ duration: 0.6 }}
-            className="text-base text-gray-300 mb-5 italic"
-          >
-            TradeTalent connects you with skilled freelancers ready to turn your
-            ideas into results.
-          </motion.p>
-          <motion.div variants={fadeInUp} transition={{ duration: 0.6 }}>
-            <SearchBar></SearchBar>
-          </motion.div>
+    <section className="px-4">
+      <div
+        className={`relative main-container mt-5 rounded-2xl min-h-[80vh] h-full bg-[url(/images/hero-bg-2.jpg)] dark:bg-[url(/images/hero-bg.jpg)] bg-center bg-cover bg-no-repeat bg-blend-soft-light dark:scale-x-[-1] flex justify-center items-center text-center`}
+      >
+        <div class="absolute inset-0 bg-linear-to-r  from-black/60 to-black/60 rounded-2xl"></div>
+        <div className="relative z-20 dark:scale-x-[-1]">
           <motion.div
-            variants={fadeInUp}
-            transition={{ duration: 0.6 }}
-            className="flex gap-4 mt-5"
+            variants={staggerContainer}
+            initial="start"
+            animate="end"
+            className="max-w-[700px] w-full my-20"
           >
-            <Button className="px-8 py-6 text-base">Post a Job</Button>
-            <Button
-              className="px-8 py-6 text-white border-white text-base"
-              variant="outline"
+            <motion.h1
+              variants={fadeInUp}
+              transition={{ duration: 0.6 }}
+              className="text-7xl font-semibold text-white mb-2"
             >
-              Join as Freelancer
-            </Button>
+              Find Talent.{" "}
+              <span className="bg-linear-to-r from-emerald-400 to-green-600 bg-clip-text text-transparent italic drop-shadow-[0_2px_6px_rgba(16,185,129,0.4)]">
+                Build Success.
+              </span>
+              Faster.
+            </motion.h1>
+            <motion.p
+              variants={fadeInUp}
+              transition={{ duration: 0.6 }}
+              className="text-base text-gray-300 mb-5 italic"
+            >
+              TradeTalent connects you with skilled freelancers ready to turn
+              your ideas into results.
+            </motion.p>
+            <motion.div variants={fadeInUp} transition={{ duration: 0.6 }}>
+              <SearchBar></SearchBar>
+            </motion.div>
+            <motion.div
+              variants={fadeInUp}
+              transition={{ duration: 0.6 }}
+              className="flex gap-4 mt-5 justify-center"
+            >
+              <Button className="px-8 py-6 text-base">Post a Job</Button>
+              <Button
+                className="px-8 py-6 text-white border-white text-base"
+                variant="outline"
+              >
+                Join as Freelancer
+              </Button>
+            </motion.div>
           </motion.div>
-        </motion.div>
+        </div>
       </div>
     </section>
   );
