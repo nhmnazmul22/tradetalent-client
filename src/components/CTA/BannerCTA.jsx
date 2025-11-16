d1ec05bae365c8abb5c5ed0dc57c3c3bb65b78a1
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-11-16 08:20:07 +0600

    feat: enhance BannerCTA component with animations and new button for seller registration

--- src/components/CTA/BannerCTA.jsx
+++ src/components/CTA/BannerCTA.jsx
@@ -1,23 +1,63 @@
 import React from "react";
 import Avatar from "@/assets/avater.png";
 import { CheckCircle } from "lucide-react";
+import Button from "../common/Button";
+import { motion } from "motion/react";
+import {
+  fadeInLeft,
+  fadeInRight,
+  fadeInUp,
+  staggerContainer,
+} from "@/lib/motionVariants";
 
 const BannerCTA = () => {
   return (
-    <div className="grid grid-cols-2 gap-10 items-center max-w-6xl mx-auto">
-      <figure className="overflow-hidden w-full h-[300px] rounded-2xl">
+    <motion.div
+      variants={staggerContainer}
+      initial="start"
+      whileInView="end"
+      viewport={{ once: true }}
+      className="grid md:grid-cols-2 gap-10 items-center max-w-6xl mx-auto"
+    >
+      <motion.figure
+        variants={fadeInRight}
+        initial="start"
+        whileInView="end"
+        viewport={{ once: true }}
+        className="overflow-hidden w-full h-[300px] rounded-2xl"
+      >
         <img
           src={Avatar}
           alt="Coding illustration"
           className="w-full h-full object-cover object-center"
         />
-      </figure>
+      </motion.figure>
       <div>
-        <h3 className="text-5xl font-semibold">Work Your Way</h3>
-        <p className="not-dark:text-gray-700 mt-2">
+        <motion.h3
+          variants={fadeInLeft}
+          initial="start"
+          whileInView="end"
+          viewport={{ once: true }}
+          className="text-4xl sm:text-5xl font-semibold"
+        >
+          Work Your Way
+        </motion.h3>
+        <motion.p
+          variants={fadeInLeft}
+          initial="start"
+          whileInView="end"
+          viewport={{ once: true }}
+          className="not-dark:text-gray-700 mt-2"
+        >
           You bring the skill. We'll make earning easy.
-        </p>
-        <ul className="mt-5 flex flex-col gap-2">
+        </motion.p>
+        <motion.ul
+          variants={fadeInLeft}
+          initial="start"
+          whileInView="end"
+          viewport={{ once: true }}
+          className="mt-5 flex flex-col gap-2"
+        >
           <li className="flex gap-2 items-center text-base">
             <span>
               <CheckCircle size={18} className="text-green-500"></CheckCircle>
@@ -42,9 +82,17 @@ const BannerCTA = () => {
             </span>
             <span>24/7 support</span>
           </li>
-        </ul>
+        </motion.ul>
+        <motion.div
+          variants={fadeInLeft}
+          initial="start"
+          whileInView="end"
+          viewport={{ once: true }}
+        >
+          <Button className="mt-5 cursor-pointer w-fit">Become a Seller</Button>
+        </motion.div>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
