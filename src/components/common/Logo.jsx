5aa3963da151538f6b29191f3064af1b92d7f08f
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-11-16 08:21:16 +0600

    refactor: rename Logo component to Logo_v2 and update structure and styles

--- src/components/common/Logo.jsx
+++ src/components/common/Logo.jsx
@@ -1,17 +1,23 @@
 import { Laptop } from "lucide-react";
 import React from "react";
 
-const Logo = () => {
+const Logo_v2 = ({ logoTextColor }) => {
   return (
-    <div className="text-black dark:text-white flex items-center gap-2">
-      <span className="inline-block bg-linear p-1 rounded-md text-white">
-        <Laptop size={28} className=""></Laptop>
-      </span>
-      <h2 className="text-2xl font-bold">
-        Trade<span className="italic">Talent</span>
-      </h2>
+    <div className="flex items-center gap-2 text-black dark:text-white">
+      <div className="bg-linear text-white p-2 rounded-lg flex items-center justify-center">
+        <Laptop size={20} />
+      </div>
+
+      <div className="flex flex-col gap-0">
+        <h2 className={`text-lg font-bold ${logoTextColor}`}>
+          Trade<span className="italic text-blue-600">Talent</span>
+        </h2>
+        <span className="text-xs opacity-70 italic text-gray-700 dark:text-gray-200">
+          Empowering Digital Skills
+        </span>
+      </div>
     </div>
   );
 };
 
-export default Logo;
+export default Logo_v2;
