3cd525c3260564686f95028d6298140a7ef1d1f9
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-11-17 09:04:00 +0600

    fix: update link destination in NavigationMenu to use href instead of label

--- src/components/Navbar/NavigationMenu.jsx
+++ src/components/Navbar/NavigationMenu.jsx
@@ -45,7 +45,7 @@ function Navigation() {
                 className={navigationMenuTriggerStyle()}
               >
                 <Link
-                  to={item.label}
+                  to={item.href}
                   className="flex flex-row gap-2 items-center"
                 >
                   {item.label}
