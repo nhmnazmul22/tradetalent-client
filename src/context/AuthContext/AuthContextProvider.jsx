a7af24f68d11f4630611cd41b5c7fd7ba607bdfa
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-11-22 09:32:24 +0600

    added auth context and firebase functunlty

---
+++ src/context/AuthContext/AuthContextProvider.jsx
@@ -0,0 +1,79 @@
+import { useEffect, useState } from "react";
+import AuthContext from "./AuthContext";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import auth from "../../firebase/firebase.init";
+import { GoogleAuthProvider } from "firebase/auth";
+import Loader from "@/components/Fallback/Loader";
+
+const provider = new GoogleAuthProvider();
+
+const AuthContextProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    console.log("Creating user with email:", email);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signinUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    return signInWithPopup(auth, provider);
+  };
+
+  const signOutUser = () => {
+    return signOut(auth);
+  };
+
+  const updateUser = (data) => {
+    return updateProfile(auth.currentUser, {
+      displayName: data.displayName,
+      photoURL: data.photoURL,
+    });
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+      }
+      setLoading(false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  if (loading) {
+    return <Loader></Loader>;
+  }
+
+  const authInfo = {
+    createUser,
+    signinUser,
+    signInWithGoogle,
+    signOutUser,
+    updateUser,
+    setLoading,
+    setUser,
+    loading,
+    user,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthContextProvider;
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,16 @@ import { RouterProvider } from "react-router/dom";
 import router from "./routes/route";
 import "./index.css";
 import ThemeProvider from "@/context/ThemeContext/ThemeContextProvider";
+import AuthContextProvider from "./context/AuthContext/AuthContextProvider";
+import { Toaster } from "react-hot-toast";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="light" storageKey="trade-talent-theme">
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <AuthContextProvider>
+      <ThemeProvider defaultTheme="light" storageKey="trade-talent-theme">
+        <RouterProvider router={router} />
+      </ThemeProvider>
+      <Toaster position="top-left" reverseOrder={false} />
+    </AuthContextProvider>
   </StrictMode>
 );
