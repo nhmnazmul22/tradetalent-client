581bf3c6fcd0fcb1ffb056e0422e5f180ae0d88f
nhmnazmul22 <najmulmonir12345@gmail.com>
2025-11-22 09:33:06 +0600

    added signup method with firebase

--- src/components/Form/SignupForm.jsx
+++ src/components/Form/SignupForm.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import Button from "../common/Button";
 import InputBox from "../common/InputBox";
-import { Label } from "../ui/label";
-import { Checkbox } from "../ui/checkbox";
-import { Link } from "react-router";
+import { Spinner } from "../ui/spinner";
 
-const SignupForm = () => {
+const SignupForm = ({ loading, type, setType, handleSubmit }) => {
   return (
-    <form className="mt-5">
+    <form className="mt-5" onSubmit={handleSubmit}>
       <div className="grid grid-cols-2 gap-5 ">
         <Button
           type="button"
-          className="bg-gray-200 text-black rounded text-base hover:text-white"
+          className={`${
+            type === "seller"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-black "
+          } rounded text-base hover:text-white`}
+          onClick={() => setType("seller")}
         >
           Seller
         </Button>
         <Button
           type="button"
-          className="bg-gray-200 text-black rounded text-base hover:text-white"
+          className={`${
+            type === "buyer"
+              ? "bg-blue-500 text-white"
+              : "bg-gray-200 text-black"
+          }  rounded text-base hover:text-white`}
+          onClick={() => setType("buyer")}
         >
           Buyer
         </Button>
@@ -28,43 +36,63 @@ const SignupForm = () => {
             label="First Name"
             isRequired={true}
             placeholder="Enter your first name"
+            name="firstName"
+            type="text"
           />
           <InputBox
             label="Last Name"
             isRequired={false}
             placeholder="Enter your Last name"
+            name="lastName"
+            type="text"
           />
         </div>
+        <InputBox
+          label="Photo URL"
+          isRequired={false}
+          placeholder="Enter your photo URL"
+          name="photoURL"
+          type="url"
+        />
         <InputBox
           label="Username"
           isRequired={true}
           placeholder="Enter your username"
+          name="username"
+          type="text"
+        />
+        <InputBox
+          label="Phone Number"
+          isRequired={false}
+          placeholder="Enter your phone number"
+          type="tel"
+          name="phone"
         />
         <InputBox
           label="Email"
           isRequired={true}
           placeholder="Enter your email"
           type="email"
-        />
-        <InputBox
-          label="Phone Number"
-          isRequired={true}
-          placeholder="Enter your phone number"
+          name="email"
         />
         <InputBox
           label="Password"
           isRequired={true}
           placeholder="Enter your Password"
+          type="password"
+          name="password"
         />
 
-        <div className="flex gap-2 items-center mt-2">
-          <Checkbox></Checkbox>
-          <Label>
-            Accept the <Link>Terms</Link> and <Link>Privacy Policy</Link>
-          </Label>
-        </div>
-
-        <Button variant="outline" className="mt-2">Signup</Button>
+        <Button type="submit" variant="outline" className="mt-2">
+          {loading ? (
+            <span className="flex gap-2 items-center">
+              <Spinner className="size-6" />
+              <span className="ms-2">Creating...</span>
+            </span>
+          ) : (
+            "Create Account"
+          )}
+        </Button>
       </div>
     </form>
   );
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,36 @@
 import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const validatePassword = (password) => {
+  const isUpperCase = /^(?=.*[A-Z])/;
+  const isLowerCase = /^(?=.*[a-z])/;
+  if (!isUpperCase.test(password)) {
+    return {
+      success: false,
+      message: "Must have an Uppercase letter in the password",
+    };
+  }
+
+  if (!isLowerCase.test(password)) {
+    return {
+      success: false,
+      message: "Must have a Lowercase letter in the password",
+    };
+  }
+
+  if (password.length <= 6) {
+    return {
+      success: false,
+      message: "Password length must be at least 6 character",
+    };
+  }
+
+  return {
+    success: true,
+    message: "Password valid",
+  };
+};
